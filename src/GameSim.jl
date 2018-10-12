6ca7b799fca111ce01f4c6b8e1e3e8962df2666f
Kyle Moore <moore.kyle39@gmail.com>
2018-10-13 01:34:16 +0200

    Sketch Game function

--- src/GameSim.jl
+++ src/GameSim.jl
@@ -2,8 +2,20 @@ module GameSim
 export Game
 
 function Game(order::Array)
-    runs = rand(1:14, 1)
-    return runs[1]
+    runs = 0
+    inning = 0
+
+    while inning < 10
+        basecd = 0
+        outs = 0
+
+        while outs != 3
+            #cycle lineup
+            #get outcome from batters talent
+            #update game state
+        end
+    end
+
 end
 
 end
