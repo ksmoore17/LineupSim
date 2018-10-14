0794bca3e6ab56cf10fb6ca40bfb1a4011a6c9ec
Kyle Moore <moore.kyle39@gmail.com>
2018-10-14 19:24:39 +0200

    Fix bugs and simplify for batter gen

--- src/Team.jl
+++ src/Team.jl
@@ -1,54 +1,65 @@
-import DataFrames
-
 module Team
-export CreateTeam()
+export CreateTeam
 
 include("./Batter.jl")
+import DataFrames
 
 function CreateTeam()
     team = Vector{Dict}(undef, 9)
 
-    talentframe = DataFrames.DataFrame(csv.file("../data/talent/talent.csv"))
-    talentfreqs = Dict{String, Array}()
+    talentframe = DataFrames.DataFrame(CSV.File(joinpath(@__DIR__,
+        "..",
+        "data",
+        "talent",
+        "talent.csv")))
+    talentfreqs = Dict{String, Tuple}()
 
-    binamount = round(log(2, length(talentframe[1])) + 1)
+    binamount = convert(Int, round(log(2, length(talentframe[1])) + 1))
 
     for statname in names(talentframe)
-        talentfreqs[statname] = TalentFreq(talentframe[:statname], binamount)
+        talentfreqs[string(statname)] = TalentFreq(talentframe[statname], binamount)
+    end
 
     for i in 1:9
-        team[i] = CreateBatter(talentfreqs)
-
+        team[i] = Batter.CreateBatter(talentfreqs)
+    end
     return team
 end
 
-function OrderBans()
+function OrderBans(team)
 
 end
 
 function TalentFreq(talentdist, binamount)
-    binwidth = (max(talentdist) - min(talentdist)) / binamount
+    statmax = maximum(talentdist) + .0000001
+
+    binwidth = (statmax - minimum(talentdist)) / binamount
     binbreaks = Vector{}(undef, binamount + 1)
+    talentbinfreqs = Vector{}(undef, binamount)
+    talentbinbounds = Vector{}(undef, binamount)
 
-    binbreaks[1] = min(talentdist)
-    binbreaks[binamount] = max(talentdist)
+    binbreaks[1] = minimum(talentdist)
+    binbreaks[binamount + 1] = statmax
 
-    for i in 2:binamount
+    for i in 2:binamount + 1
         binbreaks[i] = ((i - 1) * binwidth) + binbreaks[1]
     end
-
-    talentbinfreqs = Vector{}(undef, binamount - 1)
 
     for i in 1:length(binbreaks) - 1
         count = 0
         lowerbound = binbreaks[i]
         upperbound = binbreaks[i + 1]
         for stat in talentdist
-            lowerbound < stat < upperbound ? count += 1 : continue
+            lowerbound <= stat < upperbound ? count += 1 : continue
         end
-        talentbinfreqs[i] = ((lowerbound, upperbound), count)
+        talentbinbounds[i] = (lowerbound, upperbound)
+        talentbinfreqs[i] = count
     end
-end
 
+    total = sum(talentbinfreqs)
+    talentbinfreqs = talentbinfreqs / total
+
+    return (talentbinbounds, talentbinfreqs)
+end
 
 end
