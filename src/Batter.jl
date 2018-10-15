4a3ec342c31d2a95869fb9b62f05cd8a395a5cce
Kyle Moore <moore.kyle39@gmail.com>
2018-10-16 00:16:30 +0200

    Change dict to symbol keys

--- src/Batter.jl
+++ src/Batter.jl
@@ -5,11 +5,11 @@ import StatsBase
 import Distributions
 
 function CreateBatter(talentdists)
-    player = Dict{String, Number}()
+    player = Dict{Symbol, Number}()
 
     for (talent, talentdist) in pairs(talentdists)
         talentrange = StatsBase.sample(talentdist[1], StatsBase.FrequencyWeights(talentdist[2]))
-        player[talent] = rand(Distributions.Uniform(talentrange[1], talentrange[2]))
+        player[Symbol(talent)] = rand(Distributions.Uniform(talentrange[1], talentrange[2]))
     end
 
     return(player)
