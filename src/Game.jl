3a841aa4d45ae296540fd4152d41ea9101223db1
Kyle Moore <moore.kyle39@gmail.com>
2018-10-19 21:22:14 +0200

    Merge game and gamesim

---
+++ src/Game.jl
@@ -0,0 +1,21 @@
+module Game
+export Sim
+
+function Sim(order::Array)
+    runs = 0
+    inning = 0
+
+    while inning < 10
+        basecd = 0
+        outs = 0
+
+        while outs != 3
+            #cycle lineup
+            #get outcome from batters talent
+            #update game state
+        end
+    end
+
+end
+
+end
