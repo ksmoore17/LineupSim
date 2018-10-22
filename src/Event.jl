988942e6e29124e492dee5ca0aacec67df62c629
Kyle Moore <moore.kyle39@gmail.com>
2018-10-23 00:38:44 +0200

    Initial commit and sketch

---
+++ src/Event.jl
@@ -0,0 +1,16 @@
+module Event
+import CSV
+
+function EventFreq()
+    #create matrix of events with event code, initial outs, initial base code, containing outcomes + freqs
+end
+
+function PlateAppearance()
+    #generate event from game state and return outcomes
+end
+
+function NonBatter()
+    #generate nbe based on game state
+end
+
+end
