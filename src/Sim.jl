55f6a0ab5310b2006db3433be19ddf02092a24e9
Kyle Moore <moore.kyle39@gmail.com>
2018-10-27 22:30:35 +0200

    Merge Sim and game

--- src/Sim.jl
+++ src/Sim.jl
@@ -1,14 +1,15 @@
-module Sim
+module LineupSim
 
-import Combinatorics
+import Combinatorics, DataFrames, CSV, StatsBase, Distributions
 
-include("./Team.jl")
+include("team.jl")
+include("event.jl")
 
-function Sim(seasons::Int = 1, games::Int = 162)
-    team = Team.Create()
+function sim(seasons::Int = 1, games::Int = 162)
+    team = Team.create()
     teamruns = Dict{Array, Matrix}()
 
-    (lowteambans, highteambans) = BanBatters(team)
+    (lowteambans, highteambans) = banbatters(team)
 
     orderrunslist = Vector{Tuple}(undef, 10)
     runslist = Vector{Int}(undef, games * seasons)
@@ -18,7 +19,7 @@ function Sim(seasons::Int = 1, games::Int = 162)
             continue
         else
             for game in 1:games * seasons
-                runslist[game] = Game.Sim(order, team)
+                runslist[game] = gamesim(order, team)
             end
         end
         orderrunslist(Tuple(order..., runslist))
@@ -26,7 +27,32 @@ function Sim(seasons::Int = 1, games::Int = 162)
 
 end
 
-function BanBatters(team::Array, banbest::Int = 3, banworst::Int = 3)
+function gamesim(order::Array, team::Array)
+    runs = 0
+    inning = 0
+    b = 1
+
+    events = Event.eventsfreqs()
+
+    while inning < 10
+        basecd = 0
+        outs = 0
+
+        while outs != 3
+            (outsinc, basecd, runsinc) = plateappearance(order[b], events, basecd, outs)
+            runs += runsinc
+            outs += outsinc
+            (basecd, outs) = Event.nonbatter(basecd)
+            b = (b % 9) + 1
+        end
+
+        inning += 1
+    end
+
+    return runs
+end
+
+function banbatters(team::Array, banbest::Int = 3, banworst::Int = 3)
     lowteambans = Vector{}()
     highteambans = Vector{}()
     bannedorders = Vector{Vector}()
@@ -34,7 +60,7 @@ function BanBatters(team::Array, banbest::Int = 3, banworst::Int = 3)
     idwoba = Dict{}()
 
     for i in 1:length(team)
-        idwoba[i] = team[i][:wOBA]
+        idwoba[i] = team[i][2][:wOBA]
     end
 
     lowwOBAbans = sort(collect(values(idwoba)))[1:3]
